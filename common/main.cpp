216ddfcb9fc4acf70cf8f0b631a7d7ab4b2052cf
Rob Bresseleers <rob.bresseleers@vitibot.fr>
2020-12-30 12:35:31 +0100

    Added pre-commit hooks

--- common/main.cpp
+++ common/main.cpp
@@ -25,11 +25,11 @@
 static THD_WORKING_AREA(waThread1, 128);
 static THD_FUNCTION(Thread1, arg) {
 
-  (void)arg;
-  chRegSetThreadName("blinker");
-  while (true) {
-    chThdSleepMilliseconds(500);
-  }
+    (void)arg;
+    chRegSetThreadName("blinker");
+    while (true) {
+        chThdSleepMilliseconds(500);
+    }
 }
 
 /*
@@ -37,26 +37,26 @@ static THD_FUNCTION(Thread1, arg) {
  */
 int main(void) {
 
-  /*
+    /*
    * System initializations.
    * - HAL initialization, this also initializes the configured device drivers
    *   and performs the board-specific initializations.
    * - Kernel initialization, the main() function becomes a thread and the
    *   RTOS is active.
    */
-  halInit();
-  chSysInit();
+    halInit();
+    chSysInit();
 
-  /*
+    /*
    * Creates the blinker thread.
    */
-  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 
-  /*
+    /*
    * Normal main() thread activity, in this demo it does nothing except
    * sleeping in a loop and check the button state.
    */
-  while (true) {
-    chThdSleepMilliseconds(500);
- }
+    while (true) {
+        chThdSleepMilliseconds(500);
+    }
 }
