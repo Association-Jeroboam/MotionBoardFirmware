0085ced6cfb34e8a78d8165b754262021b29cb03
Apache <tj.thomas.jeantet@gmail.com>
2021-02-01 20:27:18 +0100

    Added PID class

---
+++ common/PID.h
@@ -0,0 +1,23 @@
+#ifndef MOTIONBOARDFIRMWARE_PID_H
+#define MOTIONBOARDFIRMWARE_PID_H
+
+class PID {
+public:
+    PID(float p, float i, float d, float bias, float frequency);
+    PID();
+    void reset();
+    void set(float p, float i, float d, float bias, float frequency);
+    void set(float p, float i, float d);
+    float compute(float error);
+    void setMaxIntegral(float maxIntegral);
+
+private:
+    float m_p, m_i, m_d, m_bias, m_frequency;
+    float m_errorSum, m_lastError;
+    float m_output;
+    float m_maxIntegral;
+    bool  m_initDone;
+};
+
+
+#endif //MOTIONBOARDFIRMWARE_PID_H
