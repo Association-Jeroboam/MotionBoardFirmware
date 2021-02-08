bcd0553253f8afb2ad386c06feb962c6905284b1
Apache <tj.thomas.jeantet@gmail.com>
2021-02-08 22:28:30 +0100

    [Motor] format

--- common/HAL/Motor.cpp
+++ common/HAL/Motor.cpp
@@ -8,7 +8,7 @@
 using namespace Board::IO;
 
 Motor::Motor(enum encoder encoder,
-             enum motor motor):m_encoder(encoder), m_motor(motor){}
+             enum motor motor) : m_encoder(encoder), m_motor(motor) {}
 
 void Motor::updateControl() {
     updateSpeed();
@@ -17,18 +17,18 @@ void Motor::updateControl() {
     setMotorDutyCycle(m_motor, command);
 }
 
-void Motor::updateSpeed(){
+void Motor::updateSpeed() {
     int16_t encoderCount = getEncoderCount(m_encoder);
     float motor_speed = float(encoderCount) * (1. / ENCODER_TICK_PER_TURN) * MOTOR_CONTROL_LOOP_FREQ;
     float wheel_speed = motor_speed * GEAR_RATIO;
     m_speed = wheel_speed * 2. * M_PI;
 }
 
-void Motor::setPID(float p, float i, float d, float bias, float frequency){
+void Motor::setPID(float p, float i, float d, float bias, float frequency) {
     m_speedPID.set(p, i, d, bias, frequency);
 }
 
-void Motor::setPID(float p, float i, float d){
+void Motor::setPID(float p, float i, float d) {
     m_speedPID.set(p, i, d);
 }
 
@@ -36,6 +36,6 @@ void Motor::setSpeed(float speed) {
     m_speedSetpoint = speed;
 }
 
-float Motor::getSpeed(){
+float Motor::getSpeed() {
     return m_speed;
 }
--- common/HAL/Motor.hpp
+++ common/HAL/Motor.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "PID.h"
 #include "Parameters.hpp"
 #include "BuildConf.hpp"
@@ -7,19 +8,23 @@ class Motor {
 public:
     Motor(enum Board::IO::encoder encoder,
           enum Board::IO::motor motor);
+
     void updateControl();
 
     void setSpeed(float speed);
+
     float getSpeed();
+
     void setPID(float p, float i, float d, float bias, float frequency);
+
     void setPID(float p, float i, float d);
 
 private:
     float m_speedSetpoint; // [rad/s]
     float m_speed;         // [rad/s]
-    PID   m_speedPID;
+    PID m_speedPID;
     const enum Board::IO::encoder m_encoder;
-    const enum Board::IO::motor   m_motor;
+    const enum Board::IO::motor m_motor;
 
     void updateSpeed();
 };
