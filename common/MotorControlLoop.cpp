2263c79bc6175f574d97e221e2a8a5479be0678d
Thomas Jeantet <tj.thomas.jeantet@gmail.com>
2021-02-12 17:00:47 +0100

    [MotorControlLoop] Using Data Streamer

--- common/MotorControlLoop.cpp
+++ common/MotorControlLoop.cpp
@@ -1,6 +1,7 @@
 #include "MotorControlLoop.hpp"
 #include "Logging.hpp"
 #include "Parameters.hpp"
+#include "DataStreamer.hpp"
 
 constexpr uint16_t TOGGLE_FREQUENCY = 10; //[Hz]
 
@@ -17,19 +18,21 @@ void MotorControlLoop::main() {
     Logging::println("[Motor Control Loop] init");
     setName("Motor Control Loop");
 
+
     Board::Events::eventRegister(this, Board::Events::RUN_MOTOR_CONTROL);
     Board::Events::startMotorControlLoop(MOTOR_CONTROL_LOOP_FREQ);
 
     static uint16_t toggleCounter = 0;
 
-    m_leftMotor.setSpeed(3.1415);
-    m_rightMotor.setSpeed(3.1415);
+    motorSetSpeed(LEFT_MOTOR, 3.1415);
+    motorSetSpeed(RIGHT_MOTOR, 3.1415);
 
     while (!shouldTerminate()) {
         waitOneEvent(Board::Events::RUN_MOTOR_CONTROL);
         m_leftMotor.updateControl();
         m_rightMotor.updateControl();
-
+        DataStreamer::instance()->setEntry(leftWheelSpeedEnum, m_leftMotor.getSpeed());
+        DataStreamer::instance()->setEntry(rightWheelSpeedEnum, m_rightMotor.getSpeed());
         toggleCounter++;
         if (toggleCounter % (MOTOR_CONTROL_LOOP_FREQ / TOGGLE_FREQUENCY) == 0) {
             toggleLED();
@@ -44,9 +47,11 @@ void MotorControlLoop::motorSetSpeed(enum motor motor, float speed) {
     switch (motor) {
         case LEFT_MOTOR:
             m_leftMotor.setSpeed(speed);
+            DataStreamer::instance()->setEntry(leftWheelSpeedSetpointEnum, speed);
             break;
         case RIGHT_MOTOR:
             m_rightMotor.setSpeed(speed);
+            DataStreamer::instance()->setEntry(rightWheelSpeedSetpointEnum, speed);
             break;
     }
 }
