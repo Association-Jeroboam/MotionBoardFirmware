116b34c00133fafc50a85172e772d797fe8fac42
Apache <tj.thomas.jeantet@gmail.com>
2021-02-27 18:06:56 +0100

    Fix makefiles chain

--- firmware/src/ports.mk
+++
@@ -1,9 +0,0 @@
-ifeq ($(PLATFORM),STM32G4)
-  include $(COMMONDIR)/HAL/ports/STM32G4/platform.mk
-else ifeq ($(PLATFORM),STM32F303)
-	include $(COMMONDIR)/HAL/ports/STM32F303/platform.mk
-else ifeq ($(PLATFORM),Simulator)
-  include $(COMMONDIR)/HAL/ports/Simulator/platform.mk
-else
-  $(error Invalid platform: "$(PLATFORM)")
-endif
