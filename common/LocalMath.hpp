2fda0bdc6207314efbade7abf2aadc21df3a7d73
Apache <tj.thomas.jeantet@gmail.com>
2021-02-28 15:32:55 +0100

    [LocalMath] Added angle unwrap function

--- common/LocalMath.hpp
+++ common/LocalMath.hpp
@@ -17,3 +17,10 @@ inline float moduloPI(float angle){
         angle += M_PI;
     return angle;
 }
+
+// Thanks to https://stackoverflow.com/users/1934293/roeeb
+inline float unwrap(float previous_angle, float new_angle) {
+    float d = new_angle - previous_angle;
+    d = d > M_PI ? d - 2 * M_PI : (d < -M_PI ? d + 2 * M_PI : d);
+    return previous_angle + d;
+}
