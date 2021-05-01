511ca76eb6b5ca514b7df7bc39618a4a32e4af3a
Apache <tj.thomas.jeantet@gmail.com>
2021-05-01 18:58:28 +0200

    [CAN] Building files

---
+++ firmware/libs/libs.mk
@@ -0,0 +1,5 @@
+COMMONINC += $(LIBDIR)/CanProtocol
+COMMONINC += $(LIBDIR)/CanProtocol/ChibiOS
+COMMONCPPSRC += $(LIBDIR)/CanProtocol/CanProtocol.cpp
+COMMONCPPSRC += $(LIBDIR)/CanProtocol/ChibiOS/CanRxThread.cpp
+COMMONCPPSRC += $(LIBDIR)/CanProtocol/ChibiOS/CanTxThread.cpp
