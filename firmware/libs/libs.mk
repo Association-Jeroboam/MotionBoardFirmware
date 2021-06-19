4af1fdd47fda066557c4596e66fb6b2de72ecc5f
Apache <tj.thomas.jeantet@gmail.com>
2021-06-19 16:46:31 +0200

    [RPLidar] added submodule

--- firmware/libs/libs.mk
+++ firmware/libs/libs.mk
@@ -1,5 +1,8 @@
 COMMONINC += $(LIBDIR)/CanProtocol
 COMMONINC += $(LIBDIR)/CanProtocol/ChibiOS
+COMMONINC += $(LIBDIR)/rplidar_arduino/RPLidarDriver/
+COMMONINC += $(LIBDIR)/rplidar_arduino/RPLidarDriver/inc
 COMMONCPPSRC += $(LIBDIR)/CanProtocol/CanProtocol.cpp
 COMMONCPPSRC += $(LIBDIR)/CanProtocol/ChibiOS/CanRxThread.cpp
 COMMONCPPSRC += $(LIBDIR)/CanProtocol/ChibiOS/CanTxThread.cpp
+COMMONCPPSRC += $(LIBDIR)/rplidar_arduino/RPLidarDriver/RPLidar.cpp
