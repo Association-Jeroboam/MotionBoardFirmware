128b2fd4c59c12b21618386b5f4039765c3b515e
Apache <tj.thomas.jeantet@gmail.com>
2021-07-01 21:50:07 +0200

    [Board] fixed motor declaration and computing

--- firmware/src/hardware/Board.cpp
+++ firmware/src/hardware/Board.cpp
@@ -87,15 +87,6 @@ void Board::IO::initTimers() {
     gptStart(&START_MATCH_TIMER, &startMatchTimerConfig);
 }
 
-void Board::IO::setMotorDutyCycle(Peripherals::Motor motor, float duty_cycle) {
-    if (duty_cycle > DEFAULT_MAX_PID_OUTPUT || duty_cycle < -DEFAULT_MAX_PID_OUTPUT)
-        return;
-    uint16_t percentage = (uint16_t)((duty_cycle / (2 * DEFAULT_MAX_PID_OUTPUT) + 0.5) * PWM_MAX_DUTY_CYCLE_VALUE);
-    pwmEnableChannel(&MOTOR_PWM_DRIVER,
-                     motor,
-                     PWM_FRACTION_TO_WIDTH(&MOTOR_PWM_DRIVER, PWM_MAX_DUTY_CYCLE_VALUE, percentage));
-}
-
 void Board::IO::deinitPWM() {
     Logging::println("PWM deinit");
     pwmDisableChannel(&MOTOR_PWM_DRIVER, Peripherals::LEFT_MOTOR);
