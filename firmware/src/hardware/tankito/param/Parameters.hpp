a68c6962ef9ca35fe07f0b3fd9f99e1ce82523f1
Thomas Jeantet <tj.thomas.jeantet@gmail.com>
2025-01-30 20:46:48 +0100

    copied robot projet to tankito

---
+++ firmware/src/hardware/tankito/param/Parameters.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <inttypes.h>
+#include "LocalMath.hpp"
+#include "RobotConf.hpp"
+
+constexpr uint16_t LED_TOGGLE_FREQUENCY = 10; //[Hz]
+
+constexpr uint16_t MOTOR_CONTROL_LOOP_FREQ = 100; //[Hz]
+constexpr float    MOTOR_CONTROL_LOOP_DT   = 1./MOTOR_CONTROL_LOOP_FREQ; //[s]
+
+constexpr float ENCODER_TICK_PER_TURN = 4096.;
+
+constexpr float DEFAULT_MAX_PID_OUTPUT      = 1.;
+constexpr float DEFAULT_MAX_PID_INTEGRAL    = 1.;
+
+constexpr uint16_t PWM_DUTY_CYCLE_PRECISION = 100000;
+
+constexpr float GEAR_RATIO     = 1.;
+constexpr float GEAR_RATIO_INV = 1.;
+
+constexpr float MAX_WHEEL_SPEED    = 1500.; //[mm/s] 
+constexpr float MAX_ANGULAR_SPEED  = 11.5; //Theoretically  11.58 if our wheel base is 259mm
+constexpr float MAX_LINEAR_ACCL    = 1000.; //[mm/s^2]
+constexpr float MAX_ANGULAR_ACCL    = 2*M_PI; //[mm/s^2]
+constexpr float MAX_WHEEL_ACCL     = 10000.; // [mm/s]œ
+
+constexpr float COMPUTE_DIRECTION_THRESHOLD = 10.; //[mm] //TODO: Change me!
+
+constexpr float ANGLE_PRECISION = 0.05; //[rad]
+constexpr float DISTANCE_PRECISION = 5;  //[mm]
+
+constexpr float CONTROL_COMMAND_TIMEOUT_S = 0.25; //[s]
---
+++ firmware/src/hardware/tankito/param/RobotConf.hpp
@@ -0,0 +1,166 @@
+#pragma once
+#include "ch.h"
+#include "hal.h"
+#include <inttypes.h>
+#include <climits>
+
+#ifdef WHITE_ROBOT
+
+constexpr float WHEEL_RADIUS_FACTOR = 1.0;
+constexpr float WHEEL_DISTANCE_FACTOR = 1.0;
+
+constexpr float WHEEL_LEFT_RADIUS  = 28.25 * WHEEL_DISTANCE_FACTOR;  //[mm] Theorical 28.25
+constexpr float WHEEL_RIGHT_RADIUS = 28.25 / WHEEL_RADIUS_FACTOR * WHEEL_DISTANCE_FACTOR;  //[mm] Theorical 28.25
+constexpr float WHEEL_BASE         = 261.2; //[mm] Theorical 266
+
+constexpr float LEFT_MOTOR_KP[3]      = {0.001,0.001,0.001};
+constexpr float LEFT_MOTOR_KI[3]      = {0.0001,0.0001,0.0001};
+constexpr float LEFT_MOTOR_KD      = 0.0;
+constexpr float LEFT_MOTOR_BIAS    = 0.0;
+
+constexpr float MOTOR_SPEED_RANGE[3]   = {10., 100., 500.};
+
+constexpr float RIGHT_MOTOR_KP[3]      = {0.001,0.001,0.001};
+constexpr float RIGHT_MOTOR_KI[3]      = {0.0001,0.0001,0.0001};
+constexpr float RIGHT_MOTOR_KD      = 0.0;
+constexpr float RIGHT_MOTOR_BIAS    = 0.0;
+
+constexpr float LINEAR_CONTROL_KP[3]      = {0.001,0.001,0.001};
+constexpr float LINEAR_CONTROL_KI[3]      = {0.0001,0.0001,0.0001};
+constexpr float LINEAR_CONTROL_KD      = 0.0;
+constexpr float LINEAR_CONTROL_BIAS    = 0.0;
+
+constexpr float ANGULAR_CONTROL_KP[3]      = {0.001,0.001,0.001};
+constexpr float ANGULAR_CONTROL_KI[3]      = {0.0001,0.0001,0.0001};
+constexpr float ANGULAR_CONTROL_KD      = 0.0;
+constexpr float ANGULAR_CONTROL_BIAS    = 0.0;
+
+constexpr float INITIAL_X_POS       = 0.;
+constexpr float INITIAL_Y_POS       = 0.;
+constexpr float INITIAL_ANGLE       = 0;
+
+constexpr float KP         = 2; //TODO: Change me!
+constexpr float KA         = 4; //TODO: Change me!
+constexpr float KB         = -4.5;// -4.5; //TODO: Change me!
+
+#define MOTOR_LEFT_FORWARD   PAL_LOW
+#define MOTOR_LEFT_BACKWARD  PAL_HIGH
+#define MOTOR_RIGHT_FORWARD  PAL_HIGH
+#define MOTOR_RIGHT_BACKWARD PAL_LOW
+
+#define LEFT_ENCODER_DRIVER QEID8
+#define RIGHT_ENCODER_DRIVER QEID2
+
+__extension__ const QEIConfig leftEncoderConf{
+    .mode        = QEI_MODE_QUADRATURE,
+    .resolution  = QEI_BOTH_EDGES,
+    .dirinv      = QEI_DIRINV_FALSE,
+    .overflow    = QEI_OVERFLOW_WRAP,
+    .min         = 0,
+    .max         = SHRT_MAX,
+    .notify_cb   = NULL,
+    .overflow_cb = NULL,
+};
+
+__extension__ const QEIConfig rightEncoderConf{
+    .mode        = QEI_MODE_QUADRATURE,
+    .resolution  = QEI_BOTH_EDGES,
+    .dirinv      = QEI_DIRINV_TRUE,
+    .overflow    = QEI_OVERFLOW_WRAP,
+    .min         = 0,
+    .max         = SHRT_MAX,
+    .notify_cb   = NULL,
+    .overflow_cb = NULL,
+};
+
+constexpr uint8_t MOTOR_LEFT_CHANNEL  = 0;
+constexpr uint8_t MOTOR_RIGHT_CHANNEL = 1;
+
+#define MOTOR_LEFT_P_CHAN_LINE  PAL_LINE(GPIOB, 4U)
+#define MOTOR_RIGHT_P_CHAN_LINE PAL_LINE(GPIOA, 7U)
+
+#define MOTOR_LEFT_P_CHAN_LINE_MODE PAL_MODE_ALTERNATE(2)
+#define MOTOR_RIGHT_P_CHAN_LINE_MODE PAL_MODE_ALTERNATE(2)
+
+#define MOTOR_LEFT_DIR_PIN PAL_LINE(GPIOB, 5U)
+#define MOTOR_LEFT_DIR_PIN_MODE PAL_MODE_OUTPUT_PUSHPULL
+#define MOTOR_RIGHT_DIR_PIN PAL_LINE(GPIOA, 5U)
+#define MOTOR_RIGHT_DIR_PIN_MODE PAL_MODE_OUTPUT_PUSHPULL
+
+
+#elif defined(BRUT_ROBOT)
+
+constexpr float WHEEL_RADIUS_FACTOR = 1.0;
+constexpr float WHEEL_DISTANCE_FACTOR = 1.0;
+
+constexpr float WHEEL_LEFT_RADIUS  = 28.25 * WHEEL_DISTANCE_FACTOR;  //[mm] Theorical 28.25
+constexpr float WHEEL_RIGHT_RADIUS = 28.25 / WHEEL_RADIUS_FACTOR * WHEEL_DISTANCE_FACTOR;  //[mm] Theorical 28.25
+constexpr float WHEEL_BASE         = 258.; //[mm] Theorical 266
+
+constexpr float LEFT_MOTOR_KP      = 0.004;
+constexpr float LEFT_MOTOR_KI      = 0.002;
+constexpr float LEFT_MOTOR_KD      = 0.0;
+constexpr float LEFT_MOTOR_BIAS    = 0.15;
+
+constexpr float RIGHT_MOTOR_KP      = 0.003;
+constexpr float RIGHT_MOTOR_KI      = 0.0022;
+constexpr float RIGHT_MOTOR_KD      = 0.0;
+constexpr float RIGHT_MOTOR_BIAS    = 0.15;
+
+constexpr float INITIAL_X_POS       = 655.; //TODO: Change me!
+constexpr float INITIAL_Y_POS       = 230.; //TODO: Change me!
+constexpr float INITIAL_ANGLE       = 0; //TODO: Change me!
+
+constexpr float KP         = 0; //TODO: Change me!
+constexpr float KA         = 0; //TODO: Change me!
+constexpr float KB         = -0.6; //TODO: Change me!
+
+#define MOTOR_LEFT_FORWARD   PAL_LOW
+#define MOTOR_LEFT_BACKWARD  PAL_HIGH
+#define MOTOR_RIGHT_FORWARD  PAL_HIGH
+#define MOTOR_RIGHT_BACKWARD PAL_LOW
+
+#define LEFT_ENCODER_DRIVER QEID2
+#define RIGHT_ENCODER_DRIVER QEID8
+
+//TODO: update
+__extension__ const QEIConfig leftEncoderConf{
+    .mode        = QEI_MODE_QUADRATURE,
+    .resolution  = QEI_BOTH_EDGES,
+    .dirinv      = QEI_DIRINV_TRUE,
+    .overflow    = QEI_OVERFLOW_WRAP,
+    .min         = 0,
+    .max         = SHRT_MAX,
+    .notify_cb   = NULL,
+    .overflow_cb = NULL,
+};
+
+//TODO: update
+__extension__ const QEIConfig rightEncoderConf{
+    .mode        = QEI_MODE_QUADRATURE,
+    .resolution  = QEI_BOTH_EDGES,
+    .dirinv      = QEI_DIRINV_FALSE,
+    .overflow    = QEI_OVERFLOW_WRAP,
+    .min         = 0,
+    .max         = SHRT_MAX,
+    .notify_cb   = NULL,
+    .overflow_cb = NULL,
+};
+
+constexpr uint8_t MOTOR_LEFT_CHANNEL  = 1;
+constexpr uint8_t MOTOR_RIGHT_CHANNEL = 0;
+
+#define MOTOR_LEFT_P_CHAN_LINE PAL_LINE(GPIOA, 7U)
+#define MOTOR_RIGHT_P_CHAN_LINE PAL_LINE(GPIOB, 4U)
+
+#define MOTOR_LEFT_P_CHAN_LINE_MODE PAL_MODE_ALTERNATE(2)
+#define MOTOR_RIGHT_P_CHAN_LINE_MODE PAL_MODE_ALTERNATE(2)
+
+#define MOTOR_LEFT_DIR_PIN PAL_LINE(GPIOA, 5U)
+#define MOTOR_LEFT_DIR_PIN_MODE PAL_MODE_OUTPUT_PUSHPULL
+#define MOTOR_RIGHT_DIR_PIN PAL_LINE(GPIOB, 5U)
+#define MOTOR_RIGHT_DIR_PIN_MODE PAL_MODE_OUTPUT_PUSHPULL
+
+#else
+#error "Define a robot!"
+#endif /* define(YELLOW_ROBOT */
